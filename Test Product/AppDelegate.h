a75a04118e45fd3e0e2f49de9cd5fe20b15a7b8f
Jeremy Davis <nooober@jeremy-daviss-macbookpro53.local>
2014-04-23 13:17:20 -0700

    Initial Commit

---
+++ Test Product/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test Product
+//
+//  Created by Jeremy Davis on 4/23/14.
+//  Copyright (c) 2014 Test Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
